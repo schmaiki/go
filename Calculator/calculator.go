12bae558e9e31c0725ac6d3c35fd8b5a8a15a5e1
Schmaiki <maikpeters1511@gmail.com>
2022-08-09 15:12:45 +0200

    verfügbaren Typen

--- Calculator/calculator.go
+++ Calculator/calculator.go
@@ -1,7 +1,20 @@
 package calculator
 
+// Packageebene Vaariable
+// PI:= 3.14159265359 funktioniert nicht
+
+var PI float64 = 3.14159265359
+
 func Add(left, right int) int {
-	return left + right
+
+	var sum int = left + right
+
+	// Möglichkeiten für Variablen
+	// var sum int
+	// var sum = left + right
+	// sum := left + right
+
+	return sum
 }
 
 func Divide(left, right int) (quotient int, remainder int) {
---
+++ types.go
@@ -0,0 +1,17 @@
+package main
+
+// Zahlen
+// int, int8, int16, int32, int64
+// uint, uint8, uint16, uint32, uint64, uintptr
+// byte (=uint8)
+// float32, float64
+//complex64, complex128
+
+// Zeichen UTF8 Zeichensatz
+// string
+// rune (= int32)
+
+// bool
+
+var i = 42
+var f = float64(i)
